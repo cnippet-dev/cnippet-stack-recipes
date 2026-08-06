56d9c3ed83567ce9f4a0392e9ece74277f718d09
AjayRajNegi <ajayrajnegi111@gmail.com>
2026-08-06 08:48:56 +0530

    refactor: prisma.ts

--- apps/prisma/lib/db/prisma.ts
+++ apps/prisma/lib/db/prisma.ts
@@ -1,17 +1,30 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@/app/generated/prisma/client";
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient;
+// TODO : see if this is necessary
+const connectionString = process.env.DATABASE_URL;
+if (!connectionString) {
+  throw new Error("DATABASE_URL is not set");
+}
+
+const globalForPrisma = globalThis as {
+  prisma?: PrismaClient;
 };
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-});
-const prisma =
-  globalForPrisma.prisma ||
+
+export const prisma =
+  globalForPrisma.prisma ??
   new PrismaClient({
-    adapter,
+    adapter: new PrismaPg({
+      connectionString: connectionString,
+    }),
+    log:
+      process.env.NODE_ENV === "development"
+        ? ["query", "warn", "error"]
+        : ["error"],
   });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
+
 export default prisma;
