c8295f3c6b31d6ed68c30393f93c0f64a854f8ba
AjayRajNegi <ajayrajnegi111@gmail.com>
2026-09-01 10:25:47 +0530

    feat: create all clients

---
+++ apps/supabase/utils/supabase/server.ts
@@ -0,0 +1,29 @@
+import { createServerClient } from "@supabase/ssr";
+import type { cookies } from "next/headers";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error("No SUPABASE_URL or SUPABASE_KEY");
+}
+
+export const createClient = (
+  cookieStore: Awaited<ReturnType<typeof cookies>>,
+) => {
+  return createServerClient(supabaseUrl, supabaseKey, {
+    cookies: {
+      getAll() {
+        return cookieStore.getAll();
+      },
+      setAll(cookiesToSet) {
+        try {
+          // biome-ignore lint/suspicious/useIterableCallbackReturn: server client
+          cookiesToSet.forEach(({ name, value, options }) =>
+            cookieStore.set(name, value, options),
+          );
+        } catch {}
+      },
+    },
+  });
+};
