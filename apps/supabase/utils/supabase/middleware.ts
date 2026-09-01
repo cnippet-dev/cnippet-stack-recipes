71604a8b24b471d3e9e51b59a772c21702ca05e2
AjayRajNegi <ajayrajnegi111@gmail.com>
2026-09-01 19:08:44 +0530

    feat: proxy to refresh the auth token

--- apps/supabase/utils/supabase/middleware.ts
+++ apps/supabase/utils/supabase/middleware.ts
@@ -8,34 +8,49 @@ if (!supabaseUrl || !supabaseKey) {
   throw new Error("No SUPABASE_URL or SUPABASE_KEY");
 }
 
-export const createClient = (request: NextRequest) => {
+const SUPABASE_URL: string = supabaseUrl;
+const SUPABASE_KEY: string = supabaseKey;
+
+export async function updateSession(request: NextRequest) {
   let supabaseResponse = NextResponse.next({
-    request: {
-      headers: request.headers,
-    },
+    request,
   });
 
-  const supabase = createServerClient(supabaseUrl, supabaseKey, {
+  const supabase = createServerClient(SUPABASE_URL, SUPABASE_KEY, {
     cookies: {
       getAll() {
         return request.cookies.getAll();
       },
-
-      setAll(cookiesToSet) {
+      setAll(cookiesToSet, headers) {
         cookiesToSet.forEach(({ name, value }) => {
           request.cookies.set(name, value);
         });
-
         supabaseResponse = NextResponse.next({
           request,
         });
-
         cookiesToSet.forEach(({ name, value, options }) => {
           supabaseResponse.cookies.set(name, value, options);
+        });
+        Object.entries(headers).forEach(([key, value]) => {
+          supabaseResponse.headers.set(key, value);
         });
       },
     },
   });
 
+  const { data } = await supabase.auth.getClaims();
+
+  const user = data?.claims;
+
+  if (
+    !user &&
+    !request.nextUrl.pathname.startsWith("/login") &&
+    !request.nextUrl.pathname.startsWith("/auth")
+  ) {
+    const url = request.nextUrl.clone();
+    url.pathname = "/login";
+    return NextResponse.redirect(url);
+  }
+
   return supabaseResponse;
-};
+}
